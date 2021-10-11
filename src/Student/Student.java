bec8d432dea7f238f552c063aa85e4d98a32f6c3
K-Sofya <sofya.kustova.2018@mail.ru>
2021-10-11 10:46:59 +0300

    add pr6

---
+++ src/Student/Student.java
@@ -0,0 +1,23 @@
+package Student;
+public class Student {
+        private int idNumber;
+        //конструктор
+        public Student(int idNumber) {
+            this.idNumber = idNumber;
+        }
+
+        //геттеры и сеттеры
+        public int getIdNumber() {
+            return idNumber;
+        }
+
+        public void setIdNumber(int idNumber) {
+            this.idNumber = idNumber;
+        }
+
+        @Override
+        public String toString() {
+            return idNumber + " ";
+        }
+    }
+
